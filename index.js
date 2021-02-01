e211a1b469c5d15eb0cd21202b0f5b4458cd6ccf
Sophie Gu <sophieqgu@gmail.com>
2021-02-01 05:32:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ BoardMember.prototype.releasePressStatement = function() {
   return "You will see great things from Scuber.";
 }
 
-BoardMember.prototype.veto = function() {
+BoardMember.prototype.sayHi = function() {
   return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
 }
